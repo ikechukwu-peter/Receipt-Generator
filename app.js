75506712c15f81657d30b87dad12c0baee633354
Peter <pitiyke@gmail.com>
2020-11-20 09:20:18 +0100

    Added search functionality

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const hbr = require("handlebars")
+// const hbr = require("handlebars")
 const mongoose = require("mongoose");
 const morgan = require("morgan")
 const dotenv = require("dotenv")
@@ -9,7 +9,7 @@ const Product = require("./model/product")
 
 const app = express()
 
-const dbURI = `mongodb+srv://${process.env.DB_ADMIN_USER}:${process.env.DB_ADMIN_PASSWORD}@cluster0.944yd.mongodb.net/Product?retryWrites=true&w=majority`;
+const dbURI = `mongodb+srv://lucifer:1234@cluster0.944yd.mongodb.net/Product?retryWrites=true&w=majority`;
 
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -37,21 +37,43 @@ app.get("/", (req, res) => {
 })
 
 
-app.get("/products", async (req, res) => {
-  // TODO Use this when you give your input box name diffierent names
+app.get("/products",  (req, res) => {
+  // TODO Use this when you give your input box name diffierent names async
   // const result = await Product.find({ title: req.body.name })
-  // TODO use this if you are using the current format of html structure
-  const result = await Product.find({ title: req.query.name[0] && req.query.name[1] })
-  console.log(req.query)
-  console.log(result)
+  // // TODO use this if you are using the current format of html structure
+  //const result = await Product.find({ title: req.query.name[0] && req.query.name[1] })
+
+
+  const product_name = req.query.name;
+  
+  //this returns all separate items in the array and then find searches for them in the database
+  product_name.forEach(title=>
+      Product.find({title})
+    
+       .then(response => {
+ //This uses array.map() to get all the product price form the product object
+    
+      const item_price = response.map(product => {
+         return (product.price);
+     });
+
+     //Converts the array returned  into a number
+     const price = Number(item_price);
+     console.log(price)
+        })
+
+      .catch(err => {
+        res.redirect("/")
+        console.log(err)
+      })
+    ) 
+
+    
 }
-
-
-
 )
 
 //Just used to add data to the database
-app.post("/genReceipt", (req, res) => {
+app.post("/add_products", (req, res) => {
   const product = new Product({
     title: "Ferrari",
     price: 56773200
